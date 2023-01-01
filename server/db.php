b4409c95b5a397ab77a78944a8085383d17d7339
chamara sandanayake <114943338+scssandanayake@users.noreply.github.com>
2023-01-01 10:48:36 +0530

    Update db.php

--- server/db.php
+++ server/db.php
@@ -1,7 +1,7 @@
 <?php
     const DB_HOST = 'localhost';
     const DB_USER = 'u190354275_root';
-    const DB_PASS = 'Chammarox123#ABC';
+    const DB_PASS = 'chammarox123#ABC';
     const DB_NAME = 'u190354275_forumdb';
 
     $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
