c20ac9a005db0919b4d801d15423070d82912a2c
scssandanayake <114943338+scssandanayake@users.noreply.github.com>
2023-01-04 14:54:51 +0530

    change the db forum details

--- server/db.php
+++ server/db.php
@@ -1,13 +1,13 @@
 <?php
     const DB_HOST = 'localhost';
-    const DB_USER = 'root';
-    const DB_PASS = '';
-    const DB_NAME = 'forumdb';
+    const DB_USER = 'u190354275_root';
+    const DB_PASS = 'chammarox123#ABC';
+    const DB_NAME = 'u190354275_forumdb';
 
     $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     if(!$connection){
         die("Connection Failed: ".mysqli_connect_error());
     } else {  
-        mysqli_select_db($connection, 'forumdb');
+        mysqli_select_db($connection, 'u190354275_forumdb');
     } 
 ?>
